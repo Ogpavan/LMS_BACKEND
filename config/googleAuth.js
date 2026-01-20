d7b94261e6974b21020cdafc907f75d3e048fe65
Ogpavan <pawantwp@gmail.com>
2026-01-21 01:46:43 +0530

    asds

--- config/googleAuth.js
+++ config/googleAuth.js
@@ -3,7 +3,7 @@ const { google } = require("googleapis");
 const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  "http://localhost:5000/api/google/callback",
+  "https://app.skillspardha.com/api/google/callback",
 );
 
 module.exports = oauth2Client;
